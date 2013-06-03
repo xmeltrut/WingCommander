2c1945afb828234806602c95776b2f7f07dc7859
Chris Worfolk <chris.worfolk@bskyb.com>
2013-06-03 12:23:18 +0100

    Replace tab indentation with spaces. Fixes #3

--- src/WingCommander.php
+++ src/WingCommander.php
@@ -11,83 +11,83 @@
 
 class WingCommander extends Mustache_Engine
 {
-	/**
-	 * Path to template files
-	 */
-	private $templatePath = "./templates";
-	
-	/**
-	 * Variables to parse in
-	 */
-	private $vars = array();
-	
-	/**
-	 * Initialise the template engine
-	 */
-	public static function init ()
-	{
-		Flight::map("render", function($template, $data, $toVar = false){
-			Flight::view()->render($template, $data, $toVar);
-		});
-		
-		Flight::register('view', 'WingCommander');
-	}
-	
-	/**
-	 * Render a page
-	 *
-	 * @param string      $filename Filename
-	 * @param array       $vars     Variables
-	 * @param string|null $toVar    If specified, it will save as a variable rather than output
-	 *
-	 * @return void
-	 */
-	public function render ($filename, $vars = array(), $toVar = null)
-	{
-		$templatePath = $this->templatePath . "/" . $filename . ".mustache";
-		
-		if (file_exists($templatePath)) {
-		
-			$template = file_get_contents($templatePath);
-			$output = parent::render($template, array_merge($this->vars, $vars));
-			
-			if ($toVar) {
-				$this->set($toVar, $output);
-			} else {
-				echo $output;
-			}
-		
-		} else {
-		
-			throw new Exception ("Template '$filename' could not be found.");
-		
-		}
-	}
-	
-	/**
-	 * Set a variable
-	 *
-	 * @param string $name  Variable name
-	 * @maram mixed  $value Value
-	 *
-	 * @return boolean Success
-	 */
-	public function set ($name, $value)
-	{
-		$this->vars[$name] = $value;
-		return true;
-	}
-	
-	/**
-	 * Set the template path
-	 *
-	 * @param string $path Path, no trailing slash
-	 *
-	 * @return boolean Success
-	 */
-	public function setTemplatePath ($path)
-	{
-		$this->templatePath = $path;
-		return true;
-	}
+    /**
+     * Path to template files
+     */
+    private $templatePath = "./templates";
+
+    /**
+     * Variables to parse in
+     */
+    private $vars = array();
+
+    /**
+     * Initialise the template engine
+     */
+    public static function init ()
+    {
+        Flight::map("render", function($template, $data, $toVar = false){
+            Flight::view()->render($template, $data, $toVar);
+        });
+
+        Flight::register('view', 'WingCommander');
+    }
+
+    /**
+     * Render a page
+     *
+     * @param string      $filename Filename
+     * @param array       $vars     Variables
+     * @param string|null $toVar    If specified, it will save as a variable rather than output
+     *
+     * @return void
+     */
+    public function render ($filename, $vars = array(), $toVar = null)
+    {
+        $templatePath = $this->templatePath . "/" . $filename . ".mustache";
+
+        if (file_exists($templatePath)) {
+
+            $template = file_get_contents($templatePath);
+            $output = parent::render($template, array_merge($this->vars, $vars));
+
+            if ($toVar) {
+                $this->set($toVar, $output);
+            } else {
+                echo $output;
+            }
+
+        } else {
+
+            throw new Exception ("Template '$filename' could not be found.");
+
+        }
+    }
+
+    /**
+     * Set a variable
+     *
+     * @param string $name  Variable name
+     * @maram mixed  $value Value
+     *
+     * @return boolean Success
+     */
+    public function set ($name, $value)
+    {
+        $this->vars[$name] = $value;
+        return true;
+    }
+
+    /**
+     * Set the template path
+     *
+     * @param string $path Path, no trailing slash
+     *
+     * @return boolean Success
+     */
+    public function setTemplatePath ($path)
+    {
+        $this->templatePath = $path;
+        return true;
+    }
 }
