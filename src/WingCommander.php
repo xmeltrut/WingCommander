4e3c637eaf0a70a0b9e991d5eea9610473f0da7d
Maximilian Sohrt <maximilian.sohrt@aida.de>
2014-11-14 13:55:02 +0100

    First step of implementing options

--- src/WingCommander.php
+++ src/WingCommander.php
@@ -24,7 +24,6 @@ class WingCommander extends Mustache_Engine
      * Variables to parse in
      */
     private $vars = array();
-
     /**
      * Initialise the template engine
      */
@@ -43,17 +42,23 @@ class WingCommander extends Mustache_Engine
      * @param string      $filename Filename
      * @param array       $vars     Variables
      * @param string|null $toVar    If specified, it will save as a variable rather than output
+     * @parma array       $options  Options for mustache renderer. Not the most beautiful way, but still a way
      *
      * @return void
      */
-    public function render ($filename, $vars = array(), $toVar = null)
+    public function render ($filename, $vars = array(), $toVar = null, $options = array())
     {
         $templatePath = $this->templatePath . '/' . $filename . '.' . $this->templateExtension;
 
         if (file_exists($templatePath)) {
 
             $template = file_get_contents($templatePath);
-            $output = parent::render($template, array_merge($this->vars, $vars));
+
+            //Old style. I did not like it, because I found no way to add options
+            //$output = parent::render($template, array_merge($this->vars, $vars));
+            //New style. I like it a bit more
+            $mustache = new parent($options);
+            $output = $mustache->render($template, array_merge($this->vars, $vars));
 
             if ($toVar) {
                 $this->set($toVar, $output);
